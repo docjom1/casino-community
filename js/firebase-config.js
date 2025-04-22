83c7e711197718d122c643337d32f3279e03a7b9
docjom1 <seojom12@gmail.com>
2025-04-22 21:49:46 +0700

    🔥 Merged login & register | Google Auth + Forgot Password + Email Verification added

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the Firebase libraries via script tag in HTML
+// firebase-app.js, firebase-auth.js, firebase-database.js will be loaded in HTML
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB0-t9Cs-Z5jITcuHPoLNDGrO9YPtRkoi4",
+    authDomain: "casinocommunity-fa874.firebaseapp.com",
+    projectId: "casinocommunity-fa874",
+    storageBucket: "casinocommunity-fa874.appspot.com",
+    messagingSenderId: "507208791665",
+    appId: "1:507208791665:web:a95bc2ea18a75ee1e427d0"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
